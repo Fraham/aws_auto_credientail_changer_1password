d6369b3069f27f9e43d85852449f90098a877237
Fraham <frahamp@gmai.com>
2022-07-12 16:42:27 +0100

    Create new access keys

--- run.ps1
+++ run.ps1
@@ -13,7 +13,7 @@ $mfaDevice = $null
 
 $1passwordRawData = $(op item get $1passwordAwsItem --vault $1passwordAwsVault --format json)
 
-if (!$?){
+if (!$?) {
     Write-Error "Something wrong"
     return
 }
@@ -22,33 +22,33 @@ $1passwordData = ($1passwordRawData | ConvertFrom-Json)
 
 $1passwordData.fields | ForEach-Object {
     #TODO: change to switch
-    if ($_.label -eq $accessKeyLabel ){
+    if ($_.label -eq $accessKeyLabel ) {
         $beforeAccessKey = $_.value
     }
-    if ($_.label -eq $secretKeyLabel){
+    if ($_.label -eq $secretKeyLabel) {
         $beforeSecretKey = $_.value
     }
-    if ($_.label -eq $totpLabel){
+    if ($_.label -eq $totpLabel) {
         $totp = $_.totp
     }
-    if ($_.label -eq $mfaDeviceLabel){
+    if ($_.label -eq $mfaDeviceLabel) {
         $mfaDevice = $_.value
     }
 }
 
-if ($null -eq $beforeAccessKey){
+if ($null -eq $beforeAccessKey) {
     Write-Error "Something wrong - no access key"
     return
 }
-if ($null -eq $beforeSecretKey){
+if ($null -eq $beforeSecretKey) {
     Write-Error "Something wrong - no secret key"
     return
 }
-if ($null -eq $totp){
+if ($null -eq $totp) {
     Write-Error "Something wrong - no totp"
     return
 }
-if ($null -eq $mfaDevice){
+if ($null -eq $mfaDevice) {
     Write-Error "Something wrong - no mfa device"
     return
 }
@@ -59,9 +59,13 @@ Write-Host $beforeSecretKey
 Write-Host $totp
 Write-Host $mfaDevice
 
+$env:AWS_ACCESS_KEY_ID = $beforeAccessKey
+$env:AWS_SECRET_ACCESS_KEY = $beforeSecretKey
+$env:AWS_SESSION_TOKEN = $null
+
 $mfaRawData = $(aws sts get-session-token --serial-number $mfaDevice --token-code $totp)
 
-if (!$?){
+if (!$?) {
     Write-Error "Error getting MFA session"
     return
 }
@@ -72,8 +76,23 @@ Write-Host $mfaCredentials
 
 $env:AWS_ACCESS_KEY_ID = $mfaCredentials.AccessKeyId
 $env:AWS_SECRET_ACCESS_KEY = $mfaCredentials.SecretAccessKey
+$env:AWS_SESSION_TOKEN = $mfaCredentials.SessionToken
 
-#TODO: create new access keys
+$createKeyRawData = $(aws iam create-access-key)
+
+if (!$?) {
+    Write-Error "Error creating new access key"
+    return
+}
+
+$createKeyData = ($createKeyRawData | ConvertFrom-Json).AccessKey
+
+$newAccessKey = $createKeyData.AccessKeyId
+$newSecretKey = $createKeyData.SecretAccessKey
+
+Write-Host $newAccessKey
+Write-Host $newSecretKey
+
 #TODO: save new access keys to 1password
 #TODO: remove the old access keys
 
